0f9a317ccbd05a3ad125c11598ecdb647e45e1c1
Solovey Aleksandr <solalex.s.a@gmail.com>
2024-10-15 20:33:53 +0300

    dev page

--- src/components/Code/Code.ts
+++ src/components/Code/Code.ts
@@ -1,17 +1,30 @@
 import hljs from "highlight.js/lib/core";
+
 import javascript from "highlight.js/lib/languages/javascript";
-import python from "highlight.js/lib/languages/python";
+import csharp from "highlight.js/lib/languages/csharp";
+import go from "highlight.js/lib/languages/go";
 import java from "highlight.js/lib/languages/java";
+import perl from "highlight.js/lib/languages/perl";
 import php from "highlight.js/lib/languages/php";
+import python from "highlight.js/lib/languages/python";
+import bash from "highlight.js/lib/languages/bash";
+import vbscript from "highlight.js/lib/languages/vbscript";
+import vbnet from "highlight.js/lib/languages/vbnet";
 import "highlight.js/styles/stackoverflow-dark.min.css";
 
 hljs.registerLanguage("javascript", javascript);
-hljs.registerLanguage("python", python);
+hljs.registerLanguage("csharp", csharp);
+hljs.registerLanguage("go", go);
 hljs.registerLanguage("java", java);
+hljs.registerLanguage("perl", perl);
 hljs.registerLanguage("php", php);
+hljs.registerLanguage("python", python);
+hljs.registerLanguage("bash", bash);
+hljs.registerLanguage("vbscript", vbscript);
+hljs.registerLanguage("vbnet", vbnet);
 
 export function initCode() {
-  document.querySelectorAll(".code .code-box").forEach((wrapper) => {
+  document.querySelectorAll(".code-box").forEach((wrapper) => {
     const copyBtn = wrapper.querySelector<HTMLElement>("[data-copy]");
     const code = wrapper.querySelector<HTMLElement>("[data-code]");
 
